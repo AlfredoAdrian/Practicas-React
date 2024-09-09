c48d2fa06ec17755902aba28925c85c0231ab71b
Alfredo AnVi <54647845+AlfredoAdrian@users.noreply.github.com>
2024-09-08 20:03:19 -0600

    Ajustes para tomar el ultimo elemento y poder moverlo a una columna del mismo color o vacias agrega botones y texto informativo

--- 01-ColorTowers/src/App.tsx
+++ 01-ColorTowers/src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "pink"];
-const InitEmptyColumn = 1;
 const heigthtowers = 15;
 const numberTowers = 7;
 
@@ -40,7 +39,13 @@ const Tower = ({ colores, indexTower, updateItemSelected }) => {
 function App() {
   const [Towers, setTowers] = useState([]);
   const [itemSelected, setItemSelected] = useState({});
+  const [InitEmptyColumn, setInitEmptyColumn] = useState(1);
 
+  // Metodos
+  const generateinitialEmptyColumn = () => {
+    let randomEmptyColumn = Math.floor(Math.random() * numberTowers);
+    setInitEmptyColumn(randomEmptyColumn);
+  };
   const createtowers = () => {
     let randomColorToewers: any = [];
     for (let i = 0; i < numberTowers; i++) {
@@ -54,38 +59,79 @@ function App() {
     }
     setTowers(randomColorToewers);
   };
-  //actualizar la torre selecioanda y la nueva seleccionada
   const updatetowerSelected = (oldaItem: any, newItem: any) => {
-    console.log("actualizando", oldaItem, newItem);
     let newTowers = [...Towers];
-    newTowers[oldaItem.tower][oldaItem.index] = newItem.color;
-    newTowers[newItem.tower][newItem.index] = oldaItem.color;
-    setTowers(newTowers);
+    let hasEmptyPosition = newTowers[newItem.tower].some((t) => t === null);
+    if (hasEmptyPosition) {
+      let emptyColumnIndex = newTowers[newItem.tower].findIndex(
+        (t) => t === null
+      );
+      if (emptyColumnIndex === 0) {
+        newTowers[oldaItem.tower][oldaItem.index] = null;
+        newTowers[newItem.tower][emptyColumnIndex] = oldaItem.color;
+      } else {
+        let previusColor = newTowers[newItem.tower][emptyColumnIndex - 1];
+        if (previusColor === oldaItem.color) {
+          newTowers[oldaItem.tower][oldaItem.index] = null;
+          newTowers[newItem.tower][emptyColumnIndex] = oldaItem.color;
+        } else {
+          setItemSelected({});
+          return;
+        }
+      }
+      setTowers(newTowers);
+      setItemSelected({});
+    } else return;
   };
   const updateItemSelected = (item: any) => {
-    // console.log("item nuevo", item, "seleccionado Actual", itemSelected);
-    if (item.tower === itemSelected.tower) {
-      return;
+    if (item.tower === itemSelected.tower) return;
+    if (item.color) {
+      let lastindex = Towers[item.tower].filter((e) => e != null).length - 1;
+      if (lastindex >= 0 && item.index !== lastindex) return;
     }
     if (itemSelected.color) updatetowerSelected(itemSelected, item);
-
     setItemSelected(item);
   };
-  // useEffect(() =>  createtowers(), []);
+  const resetGame = () => {
+    createtowers();
+    setItemSelected({});
+  };
+
+  // efectos
   useEffect(() => {
+    generateinitialEmptyColumn();
     createtowers();
     setItemSelected({});
   }, []);
-  // useEffect(() => itemSelected, []);
-  useEffect(() => {}, [itemSelected]);
+  useEffect(() => {
+    console.log("Elemento seleccionado: ", itemSelected);
+  }, [itemSelected]);
 
   return (
     <>
       <main className="App">
-        <h1>Color Towers</h1>
-        <section>
-          ¡Juega y gana! ¡No te pierdas! separara los colores en columnas, cada
-          columna tiene un color
+        <h2>Color Towers</h2>
+        <section className="Info">
+          <div className="InfoGame">
+            Ordena los colores de tus towers para ganar el juego
+          </div>
+          <div className="TowersInfo">
+            <p>Number of towers: {numberTowers}</p>
+            <p>Height of towers: {heigthtowers}</p>
+          </div>
+          <div className="ButtonsGame">
+            <button onClick={resetGame}>Reset Game</button>
+            <button onClick={generateinitialEmptyColumn}>
+              Change Empty Column
+            </button>
+          </div>
+          <div className="ItemActualinfo">
+            Color Actual
+            <div
+              className="SquareColor"
+              style={{ backgroundColor: itemSelected.color }}
+            ></div>
+          </div>
         </section>
         <section className="Game">
           {Towers.map((row, index) => {
